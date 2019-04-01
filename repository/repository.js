7e6c88d37fcc76cd2b310e55bc78a6af47d499a8
SmolnikovAM <andr.sml@gmail.com>
2019-04-01 18:15:25 +0300

    class repository erase edit append

--- repository/repository.js
+++ repository/repository.js
@@ -8,9 +8,10 @@ const {
 } = require('../helpers/queryBuilder');
 
 class Repository {
-  constructor({ from, map, strictMode }) {
+  constructor({ from, table, map, strictMode }) {
     this.dbQueryFn = dbQueryFn;
-    this.from = from;
+    this.from = from || table;
+    this.table = table;
     this.map = map;
     this.strictMode = strictMode;
 
@@ -42,23 +43,31 @@ class Repository {
 
   mapField(key, query) {
     const { alias, field, table } = this.mapFieldNames(key);
+    const args = [alias];
+    if (query) args.push(query);
     if (!table) {
       if (!(this.from instanceof Table)) {
         throw new ServerError('must be a table');
       }
-      return this.from.field(field).as(alias, query);
+      return this.from.field(field).as(...args);
     }
-    return table.field(field).as(alias, query);
+    return table.field(field).as(...args);
   }
 
-  async find(options) {
-    let { conditions, fields } = options;
-    const { pagination } = options;
-
+  async find({
+    conditions: inputConditions,
+    fields: inputFields,
+    pagination,
+    // group,
+    // order,
+  }) {
+    let conditions = null;
+    let fields = null;
     const query = new QueryBuilder(dbQueryFn);
-    if (conditions) {
-      conditions = Object.entries(conditions).map(([key, vals]) => {
-        const field = this.mapField(key);
+
+    if (inputConditions) {
+      conditions = Object.entries(inputConditions).map(([key, vals]) => {
+        const field = this.mapField(key, query);
         if (typeof vals === 'object') {
           const { operator, value } = vals;
           return [field, operator, value];
@@ -67,8 +76,8 @@ class Repository {
       });
     }
 
-    if (fields) {
-      fields = fields.map(f => this.mapField(f));
+    if (inputFields) {
+      fields = inputFields.map(f => this.mapField(f));
     } else {
       fields = [new Field(SQLCONST.STAR)];
     }
@@ -84,6 +93,68 @@ class Repository {
   async findOne(...options) {
     const [one] = await this.find(...options);
     return one;
+  }
+
+  // append single
+  async append(data) {
+    if (!(this.from instanceof Table)) {
+      throw new ServerError('not a table');
+    }
+    if (data && typeof data !== 'object') {
+      throw new LogicError('incorrect type to append');
+    }
+    const query = new QueryBuilder(dbQueryFn);
+
+    const { fields, values } = Object.entries(data).reduce(
+      (acc, [key, vals]) => {
+        const field = this.mapField(key, query);
+        acc.fields.push(field);
+        acc.values.push(vals);
+        return acc;
+      },
+      { fields: [], values: [] },
+    );
+    return query
+      .insert(this.table)
+      .into(fields)
+      .values(values)
+      .execute();
+  }
+
+  eraseById({ id }) {
+    if (!Number.isInteger(id) || id < 1) {
+      throw new LogicError('id have to be strict greater zero');
+    }
+    const query = new QueryBuilder(dbQueryFn);
+    const condition = [this.mapField('id', query), id];
+    return query
+      .delete()
+      .from(this.table)
+      .where([condition])
+      .execute();
+  }
+
+  editById({ id, ...inputSetState }) {
+    if (!Number.isInteger(id) || id < 1) {
+      throw new LogicError('id have to be strict greater zero');
+    }
+    if (!inputSetState) {
+      throw new LogicError('empty set params');
+    }
+    const query = new QueryBuilder(dbQueryFn);
+
+    const setState = Object.entries(inputSetState).map(([key, vals]) => [
+      this.mapField(key, query),
+      vals,
+    ]);
+
+    const condition = [this.mapField('id', query), id];
+
+    return query
+      .update(this.table)
+      .set(setState)
+      .where([condition])
+      .execute();
   }
 }
 
