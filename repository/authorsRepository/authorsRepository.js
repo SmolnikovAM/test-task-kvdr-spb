6b12d7796017b7d717b44e6bc15ec1c7fd55bcd2
Andrei Smolnikov <andr.sml@gmail.com>
2019-04-11 15:22:57 +0300

    author repository bugfix

--- repository/authorsRepository/authorsRepository.js
+++ repository/authorsRepository/authorsRepository.js
@@ -1,16 +1,16 @@
 const Repository = require('../repositoryClass');
 const { Table } = require('../../helpers/queryBuilder');
 
-const authorsTable = new Table('authors');
+const table = new Table('authors');
 
 const map = new Map([
-  ['id', { alias: 'id', field: 'id', authorsTable }],
-  ['author', { alias: 'author', field: 'name', authorsTable }],
+  ['id', { alias: 'id', field: 'id', table }],
+  ['author', { alias: 'author', field: 'name', table }],
 ]);
 
 class AuthorsRepository extends Repository {
   constructor(queryFn) {
-    super({ queryFn, from: authorsTable, map });
+    super({ queryFn, table, map });
   }
 }
 
